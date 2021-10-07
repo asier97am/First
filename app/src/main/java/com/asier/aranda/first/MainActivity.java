94f8c74c32f7b0d20a14b60eef104f278a24b759
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-10-07 08:43:51 +0200

    Initial commit

---
+++ app/src/main/java/com/asier/aranda/first/MainActivity.java
@@ -0,0 +1,19 @@
+package com.asier.aranda.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Asier97am
+ */
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
