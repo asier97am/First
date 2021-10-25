63194a014d11cac51ad9cf08c4945c3306d2d76b
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-10-25 13:45:47 +0200

    apartir de aqui hacer link entre activities

--- app/src/main/java/com/asier/aranda/first/LoginActivity.java
+++ app/src/main/java/com/asier/aranda/first/LoginActivity.java
@@ -3,10 +3,12 @@ package com.asier.aranda.first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 /**
  * @author Asier97am
  *
@@ -20,10 +22,24 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        ImageView mGirl=findViewById(R.id.girl);
+
+        Glide.with(this)
+                .load(R.drawable.girl)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+
+                .into(mGirl);
+
 
     }
     public void onClick(View v){
         Intent intent=new Intent(LoginActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+    public void openSignUp(View v){
+        Intent intent=new Intent(LoginActivity.this, SignUp.class);
         startActivity(intent);
     }
 }
