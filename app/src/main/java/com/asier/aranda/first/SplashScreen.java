08c4a3f5558ba0d87ac70f296e4bd3a5933e1094
Asier Aranda <asier.aranda.malagon@gmail.com>
2021-10-26 09:56:21 +0200

    signup

--- app/src/main/java/com/asier/aranda/first/SplashScreen.java
+++ app/src/main/java/com/asier/aranda/first/SplashScreen.java
@@ -2,7 +2,10 @@ package com.asier.aranda.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ImageView mBolt=findViewById(R.id.bolt);
+
+
+
+    } public void pulsarLogo(View view){
+        Intent intent=new Intent(SplashScreen.this, LoginActivity.class);
+        startActivity(intent);
+
     }
 }
